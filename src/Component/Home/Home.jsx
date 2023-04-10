8eb6ceb2cbce85f88d2137ca60f02a779b96fde5
NusratNish <108834641+NusratNish@users.noreply.github.com>
2023-04-10 20:34:50 +0600

    added data

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+
+
 
 const Home = () => {
     const tshirts = useLoaderData();
     return (
         <div>
-            <h2>home: {tshirts.length}</h2>
+            {
+              tshirts.map(tshirt => <Tshirt key={tshirt._id} tshirt={tshirt}></Tshirt>)
+            }
         </div>
     );
 };
